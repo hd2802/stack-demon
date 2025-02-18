8cf5d10a91b5926e44eefb20de32695c709758fa
Madeleine Hartland-Dykes <146767175+hd2802@users.noreply.github.com>
2025-02-18 13:59:12 +0000

    feat: adds card selection logic and animation

--- _Scripts/PlayerHand/player_hand.gd
+++ _Scripts/PlayerHand/player_hand.gd
@@ -3,10 +3,22 @@ class_name PlayerHand
 
 var cards : Array[String]
 
+var selected_card : Card = null
+
 func _ready():
 	var card : Card = load("res://_Scenes/card.tscn").instantiate()
 	card.createCard("one")
+	card.clicked.connect(_on_card_clicked)
 	self.add_child(card)
 
-func populateHand():
-	pass
+func _on_card_clicked(card: Card):
+	print("card clicked")
+	if selected_card :
+		selected_card.position.y += 20
+		
+	if selected_card != card:
+		selected_card = card
+		selected_card.position.y -= 20 
+		
+	else:
+		selected_card = null
