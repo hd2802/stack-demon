7e6b9bd1f6cc0192f25ac8ad514b398dba728588
Madeleine Hartland-Dykes <146767175+hd2802@users.noreply.github.com>
2025-02-19 07:35:22 +0000

    feat: add stack stylisation

--- CustomResources/card_data.gd
+++ CustomResources/card_data.gd
@@ -1,6 +1,5 @@
 extends Resource
 class_name CardDataResource
 
-@export var value : int
 @export var operation : String
-@export var text : String
+@export var text: String
--- _Scripts/Stack/stack.gd
+++ _Scripts/Stack/stack.gd
@@ -11,4 +11,12 @@ func _on_card_played(card: Card):
 	stack.push_back(card.card_data.operation)
 	var label : Label = Label.new()
 	label.text = card.card_data.text
+	label.set_horizontal_alignment(HORIZONTAL_ALIGNMENT_CENTER)
 	self.add_child(label)
+	self.move_child(label, 0)
+	
+	var sep_label : Label = Label.new()
+	sep_label.text = "----------"
+	sep_label.set_horizontal_alignment(HORIZONTAL_ALIGNMENT_CENTER)
+	self.add_child(sep_label)
+	self.move_child(sep_label, 0)
