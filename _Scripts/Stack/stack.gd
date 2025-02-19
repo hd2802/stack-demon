bee9b2894c6a3f6bd3676a7ac955b75cf9484d7e
Madeleine Hartland-Dykes <146767175+hd2802@users.noreply.github.com>
2025-02-19 08:08:46 +0000

    feat: add level data resource

--- _Scripts/Stack/stack.gd
+++ _Scripts/Stack/stack.gd
@@ -7,10 +7,12 @@ func _ready():
 	var player_hand = get_parent().get_node("PlayerHand")
 	player_hand.card_played.connect(_on_card_played)
 
-func _on_card_played(card: Card):
-	stack.push_back(card.card_data.operation)
+#----------------------------------------------------------------
+
+func push(value_to_push : String):
+	stack.push_back(value_to_push)
 	var label : Label = Label.new()
-	label.text = card.card_data.text
+	label.text = value_to_push
 	label.set_horizontal_alignment(HORIZONTAL_ALIGNMENT_CENTER)
 	self.add_child(label)
 	self.move_child(label, 0)
@@ -20,3 +22,12 @@ func _on_card_played(card: Card):
 	sep_label.set_horizontal_alignment(HORIZONTAL_ALIGNMENT_CENTER)
 	self.add_child(sep_label)
 	self.move_child(sep_label, 0)
+
+func initialise_stack(array : Array[String]):
+	for a in array:
+		push(a)
+
+func _on_card_played(card: Card):
+	push(card.card_data.text)
+
+#----------------------------------------------------------------
