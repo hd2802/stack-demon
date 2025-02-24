ac0802b5562b6c8bc56b3ad521a881f01def2912
Madeleine Hartland-Dykes <146767175+hd2802@users.noreply.github.com>
2025-02-24 10:53:44 +0000

    feat: add level title

--- CustomResources/level_data.gd
+++ CustomResources/level_data.gd
@@ -1,6 +1,7 @@
 extends Resource
 class_name LevelDataResource
 
+@export var level_number : int = 0
 @export var hand : Array[String] = []
 @export var start_stack : Array[String] = []
 @export var target_state : Array[String] = []
--- _Scripts/Level/level.gd
+++ _Scripts/Level/level.gd
@@ -3,13 +3,18 @@ class_name Level
 
 var level_data : LevelDataResource
 
+var title_node : Label
 var stack_node : Stack
 var hand_node : Hand
 var target_node : Target
 
 func _ready() -> void:
 	# Placeholder for testing and debugging
-	level_data = load("res://Levels/level_one.tres")
+	level_data = load("res://Levels/zero.tres")
+	
+	# Title initialisation
+	title_node = self.get_node("LevelTitleLabel")
+	title_node.text = "LEVEL " + str(level_data.level_number)
 	
 	# Stack initialisation
 	stack_node = self.get_node("Stack")
