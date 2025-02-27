dc80f3f1957b2780a00bdd2210d1de672a9ee0ee
Madeleine Hartland-Dykes <146767175+hd2802@users.noreply.github.com>
2025-02-27 11:35:34 +0000

    feat: adds level loading and generation

--- CustomResources/level_data.gd
+++ CustomResources/level_data.gd
@@ -2,6 +2,5 @@ extends Resource
 class_name LevelDataResource
 
 @export var level_number : int = 0
-@export var hand : Array[String] = []
 @export var target_value : int 
 @export var exact_target : bool 
