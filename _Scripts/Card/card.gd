db1d10e38da261511b90c7b4c9530614f27927a4
Madeleine Hartland-Dykes <146767175+hd2802@users.noreply.github.com>
2025-03-04 23:31:15 +0000

    feat: add DEL card

--- _Scripts/Card/card.gd
+++ _Scripts/Card/card.gd
@@ -12,6 +12,9 @@ func createCard(card : String):
 	self.card_data = load("res://Resources/CardData/" + card + ".tres")
 	var sprite = self.get_node("Sprite2D")
 	sprite.texture = load("res://Assets/Sprites/CardSprites/" + card + ".png")
+	if card == "del":
+		sprite.scale.x = 0.17
+		sprite.scale.y = 0.17
 
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton:
