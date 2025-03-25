27d570ad3819c724e648a25e4f59163725b07396
Madge Hartland-Dykes <146767175+hd2802@users.noreply.github.com>
2025-03-25 18:40:06 +0000

    feat: add semi-working dragging feature

--- _Scripts/CardScripts/Card/card.gd
+++ _Scripts/CardScripts/Card/card.gd
@@ -2,17 +2,21 @@ extends Node2D
 class_name Card
 
 var card_data : CardDataResource
-
 var is_hovered : bool = false
+var is_dragged : bool = false
+var drag_offset : Vector2
+var original_position : Vector2
+var is_selected : bool = false  # Track selection state
 
 var tween_hover : Tween
 var sprite : Sprite2D
 
 signal clicked(card: Node)
+signal dragged(card: Node, new_position: Vector2)
 
-func _ready() -> void:  
+func _ready() -> void:
 	sprite = self.get_node("Sprite2D")
-	print(sprite)
+	original_position = position 
 
 func createCard(card : String):
 	self.card_data = load("res://Resources/CardData/" + card + ".tres")
@@ -26,19 +30,38 @@ func set_labels():
 	top_left.text = self.card_data.text
 	bottom_right.text = self.card_data.text
 	center.text = self.card_data.text
-	
+
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventMouseButton and event.pressed:
+	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
-			clicked.emit(self)
+			if event.pressed:
+				is_dragged = true
+				drag_offset = event.position - position
+				emit_signal("clicked", self)
+			else:
+				is_dragged = false
+				emit_signal("dragged", self, position) 
 		elif event.button_index == MOUSE_BUTTON_RIGHT and get_parent().selected_card == self:
 			get_parent()._on_card_clicked(self)
-	else:
-		pass
 
 func _on_area_2d_mouse_entered() -> void:
 	is_hovered = true
 
-
 func _on_area_2d_mouse_exited() -> void:
 	is_hovered = false
+
+func _process(delta: float) -> void:
+	if is_dragged:
+		position = get_viewport().get_mouse_position() - drag_offset  
+
+		if is_selected:
+			position.y -= 20
+
+		z_index = 1 if is_selected else 0
+
+func set_selected(selected: bool) -> void:
+	is_selected = selected
+	if is_selected:
+		position.y -= 20 
+	else:
+		position.y += 20  
