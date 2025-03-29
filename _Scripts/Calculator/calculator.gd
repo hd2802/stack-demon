abf6e78c5bb6b641a81cc6f00cc6edf99c84dcb8
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-03-29 18:14:43 +0000

    feat: add more complex handling of operations in class Calculator

--- _Scripts/Calculator/calculator.gd
+++ _Scripts/Calculator/calculator.gd
@@ -1,51 +1,56 @@
 extends Node
 class_name Calculator
 
-var stack : Array[String]
+signal expression_validity(multiplier: int, is_complex: bool)
+
+var stack: Array[String]
 
 func _ready() -> void:
 	pass
-	
-# function called by the hand class -> the only connecting method
-# any other methods must be triggered through this
-func _on_hand_played(cards_played : Array[Card]) -> int:
-	
-	# initialise the stack of operands and operations 
+
+func _on_hand_played(cards_played: Array[Card]) -> int:
+	stack.clear()
+
 	for card in cards_played:
-		
 		stack.push_back(card.card_data.text)
-	
-	
+
 	var sc = evaluate()
 	return sc
 
 func evaluate() -> int:
+	var is_complex = is_complex_expression()
+	
 	if !is_valid_expression():
-		# then we want to use the highest value card that has been used
-		var m = 0
+		expression_validity.emit(1, is_complex)
+		var max_value = 0
 		for card in stack:
-			if int(card) > m:
-				m = int(card)
-		stack = []
-		return m
+			var value = int(card)
+			if value > max_value:
+				max_value = value
+		stack.clear()
+		return max_value
 	else:
-		if len(stack) == 3:
-			var operation = stack.pop_back()
-			var operand1 = stack.pop_back()
-			var operand2 = stack.pop_back()
-			print(calculate(operation, int(operand1), int(operand2)))
-			return calculate(operation, int(operand1), int(operand2))
-		# TODO deal with more complex hands played here
-		else:
-			var m = 0
-			for card in stack:
-				if int(card) > m:
-					m = int(card)
-			stack = []
-			return m
-		
+		expression_validity.emit(2 if !is_complex else 3, is_complex)  
 
-# helper functions for calculate() ------------------------------------------------------
+		if len(stack) >= 3:
+			while len(stack) >= 3:
+				var operation = stack.pop_back()
+				var operand1 = stack.pop_back()
+				var operand2 = stack.pop_back()
+				var result = calculate(operation, int(operand1), int(operand2))
+				stack.push_back(str(result))
+
+			return int(stack.pop_back())
+		else:
+			var max_value = 0
+			for card in stack:
+				var value = int(card)
+				if value > max_value:
+					max_value = value
+			stack.clear()
+			return max_value
+
+# Helper functions ------------------------------------------------------
 func is_operation(value: String) -> bool:
 	return value in ["+", "/", "X", "^", "-"]
 
@@ -53,22 +58,36 @@ func is_valid_expression() -> bool:
 	var operand_count = 0
 	
 	for token in stack:
-		if !is_operation(token):
-			operand_count += 1
-		elif is_operation(token):
+		if is_operation(token):
 			if operand_count < 2:
 				return false
 			operand_count -= 1
 		else:
-			return false
-	
+			operand_count += 1
+
 	return operand_count == 1
-	
-func calculate(operation: String, operand1 : int, operand2 : int) -> int:
+
+func is_complex_expression() -> bool:
+	# Count how many operations are in the stack
+	var operator_count = 0
+
+	for token in stack:
+		if is_operation(token):
+			operator_count += 1
+
+	return operator_count >= 2
+
+func calculate(operation: String, operand1: int, operand2: int) -> int:
 	match operation:
 		"+":
 			return operand1 + operand2
+		"-":
+			return operand1 - operand2
 		"X":
 			return operand1 * operand2
+		"/":
+			return operand1 / operand2 if operand2 != 0 else 0
+		"^":
+			return int(pow(operand1, operand2))
 		_:
 			return 0
