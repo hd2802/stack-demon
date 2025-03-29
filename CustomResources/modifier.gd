612853aeb8e06712454a2ae6953f1f712db85730
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-03-29 17:53:55 +0000

    feat: add special card and modifier resource

--- CustomResources/modifier.gd
+++ CustomResources/modifier.gd
@@ -1,1 +1,10 @@
 extends Resource
+class_name Modifier
+
+@export var name : String
+@export var description : String
+
+# used for programmer purposes
+@export var effect : String
+
+@export var sprite_path : String
--- CustomResources/special_card.gd
+++ CustomResources/special_card.gd
@@ -1,1 +1,11 @@
 extends Resource
+class_name SpecialCardData
+
+@export var name : String
+
+@export var description : String
+
+# the following are used for programmer purposes
+@export var effect : String
+
+@export var sprite_path : String
