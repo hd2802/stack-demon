ae924c841d8aeeaaac1973df6d1590870b13419a
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-03-31 13:03:37 +0100

    feat: edit level transition

--- _Scripts/Shop/shop.gd
+++ _Scripts/Shop/shop.gd
@@ -3,8 +3,28 @@ class_name Shop
 
 signal shop_finished
 
-func _ready() -> void:
-	pass
+var modifier_cards
 
+var POSSIBLE_MODIFIERS = [
+	"hand", "discard", "mult"
+]
+
+func _ready() -> void:
+	modifier_cards = $ModifierCards
+		
 func _on_continue_button_pressed() -> void:
 	shop_finished.emit()
+
+func generate_random_modifier() -> void:
+	var rand_index:int = randi() % POSSIBLE_MODIFIERS.size()
+	var random_modifier = POSSIBLE_MODIFIERS[rand_index]
+	print(random_modifier)
+	
+func _on_button_pressed() -> void:
+	generate_random_modifier()
+
+func _on_button_2_pressed() -> void:
+	generate_random_modifier()
+
+func _on_button_3_pressed() -> void:
+	generate_random_modifier()
--- _Scripts/Transitions/Level/level_transition.gd
+++ _Scripts/Transitions/Level/level_transition.gd
@@ -1,13 +1,97 @@
 extends Control
 class_name LevelTransition
 
-signal level_next()
+# TODO: edit this so that the cards are weighted properly
+# and so that it contains some interesting cards that can be added
+var CARDS : Array[String] = [
+	"ten", "ten", "ten",
+	"twenty", "twenty", "twenty",
+	"one", "one", "one",
+	"three", "three", "three",
+	"four", "two", "five", "nine",
+	"six", "seven", "nine", "nine", "two",
+	"two", "three", "one", "two", "three",
+	"four", "six", "six", "six", "seven",
+	"nine", "ten", "add", "add", "add",
+	"add", "ten", "add", "add",
+	"add", "add","add","add",
+	"add", "add", "multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply",
+	"multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply",
+	"multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply",
+	"multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply",
+	"multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply",
+	"multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply",
+	"multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply",
+	"multiply","multiply","multiply","multiply","multiply","multiply","multiply","multiply"
+]
 
-var title
+var card_list : Array[StaticCard]
+var gen_cards : Array[String]
+
+var selected_card : StaticCard 
+
+var card_container : HBoxContainer
+var card_one : StaticCard
+var card_two : StaticCard
+var card_three : StaticCard
+
+var continue_button : Button
+
+var selection_prompt : Label
+
+signal level_next 
 
 func _ready() -> void:
-	title = $Title
-	title.text = "[wave amp=25 freq=1.5] Level " + str(self.get_parent().current_level) +  " Complete[/wave]"
+	card_container = $CardContainer
+	continue_button = $ContinueButton
+	selection_prompt = $SelectionPrompt
+	
+	var round_number = self.get_parent().current_tier
+	if round_number == 0:
+		round_number += 1
+	
+	var title = $Title
+	title.text = "[wave amp=25 freq=1.5] Round "  + str(round_number) + " Complete[/wave]"
+	generate_cards()
+	
 
+func generate_cards() -> void:
+	CARDS.shuffle()
+	gen_cards.append(CARDS[0])
+	gen_cards.append(CARDS[1])
+	gen_cards.append(CARDS[2])
+
+	for card in gen_cards:
+		var new_card = load("res://_Scenes/Card/StaticCard/static_card.tscn").instantiate()
+		new_card.createCard(card)
+		new_card.clicked.connect(_on_card_clicked)
+		card_container.add_child(new_card)
+		card_list.append(new_card)
+
+func _on_card_clicked(card : StaticCard) -> void:
+	continue_button.text = "Continue"
+	selected_card = card
+	var idx = card_list.find(card)
+	
+	# visual effects
+	for static_card in card_list:
+		if selected_card != static_card:
+			card_container.remove_child(static_card)
+			static_card.queue_free()
+		else:
+			static_card.disabled = true
+			
+			var shader_rect = ColorRect.new()
+			shader_rect.set_size(Vector2(100, 150))
+			
+			var highlight_shader = load("res://Resources/Shaders/highlight.gdshader")
+			shader_rect.material = ShaderMaterial.new()
+			shader_rect.material.shader = highlight_shader
+			
+			static_card.add_child(shader_rect)
+	
+	_DeckManager.add_card_to_deck(gen_cards[idx])
+	selection_prompt.visible = false
+	
 func _on_continue_button_pressed() -> void:
 	level_next.emit()
