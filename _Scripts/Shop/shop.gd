bf65290aa762a647a62be3afc8fa3f36728d7fae
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-03-31 15:07:00 +0100

    feat: finish adding features

--- _Scripts/Shop/shop.gd
+++ _Scripts/Shop/shop.gd
@@ -3,28 +3,8 @@ class_name Shop
 
 signal shop_finished
 
-var modifier_cards
-
-var POSSIBLE_MODIFIERS = [
-	"hand", "discard", "mult"
-]
-
 func _ready() -> void:
-	modifier_cards = $ModifierCards
+	pass
 		
 func _on_continue_button_pressed() -> void:
 	shop_finished.emit()
-
-func generate_random_modifier() -> void:
-	var rand_index:int = randi() % POSSIBLE_MODIFIERS.size()
-	var random_modifier = POSSIBLE_MODIFIERS[rand_index]
-	print(random_modifier)
-	
-func _on_button_pressed() -> void:
-	generate_random_modifier()
-
-func _on_button_2_pressed() -> void:
-	generate_random_modifier()
-
-func _on_button_3_pressed() -> void:
-	generate_random_modifier()
