9149c1bed7f5c2ec24611537ec58e605800ee48c
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-03-31 18:34:37 +0100

    style: add button press audio to options

--- _Scripts/Options/options.gd
+++ _Scripts/Options/options.gd
@@ -1,25 +1,36 @@
 extends Node
 class_name Options
 
+var button_audio
+
 signal resume()
 
 func _ready() -> void:
 	get_tree().paused = true
+	button_audio = $AudioStreamPlayer2D
 
 func _on_restart_pressed() -> void:
+	button_audio.play()
+	await get_tree().create_timer(0.25).timeout
 	get_tree().paused = false
 	_GameManager.game_loop = true
 	get_tree().change_scene_to_file("res://_Scenes/LevelRunner/level_runner.tscn")
 
 func _on_resume_pressed() -> void:
+	button_audio.play()
+	await get_tree().create_timer(0.25).timeout
 	get_tree().paused = false
 	resume.emit()
 
 func _on_cross_button_pressed() -> void:
+	button_audio.play()
+	await get_tree().create_timer(0.25).timeout
 	get_tree().paused = false
 	resume.emit()
 
 func _on_main_menu_pressed() -> void:
+	button_audio.play()
+	await get_tree().create_timer(0.25).timeout
 	get_tree().paused = false
 	_GameManager.game_loop = false
 	get_tree().change_scene_to_file("res://_Scenes/MainMenu/main_menu.tscn")
